78ca447bf2741b4f3b50a77a19390bfaa6bc3ae0
JoanRecioJimenez <joamreciojimenez@gmail.com>
2026-06-30 14:49:56 -0600

    Se vuelve a hacer el git add y commit ya que hubo un error al guardar el archivo
    ya que se me olvido guardar en el NetBeans

--- src/materialesBibliograficos/MaterialBibliografico.java
+++ src/materialesBibliograficos/MaterialBibliografico.java
@@ -4,10 +4,61 @@
  */
 package materialesBibliograficos;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author Student
  */
-public class MaterialBibliografico {
+public abstract class MaterialBibliografico {
+    private int codigo;
+    private String titulo;
+    private String autor;
+    private LocalDate anoPublicacion;
+    private String categoria;
+    private boolean disponible;
     
+    public int getCodigo(){
+        return codigo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public LocalDate getAñoPublicacion() {
+        return anoPublicacion;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    public MaterialBibliografico(int codigo, String titulo, String autor, LocalDate añoPublicacion, String categoria, boolean disponible) {
+        this.codigo = codigo;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacion = añoPublicacion;
+        this.categoria = categoria;
+        this.disponible = disponible;
+    }
+
+    @Override
+    public String toString() {
+        return "MaterialBibliografico{" + "codigo=" + codigo + ", titulo=" + titulo + ", autor=" + autor + ", anoPublicacion=" + anoPublicacion + ", categoria=" + categoria + ", disponible=" + disponible + '}';
+    }
+   
+  
 }
